c18fffa0e97bc13d7ddb1fdb76f8444f875a0769
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-02-15 08:38:43 -0700

    checkbox

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import TodoList from './TodoList';
+import './Todo.css';
 
 function List(props) {
   return (
       <div className={`list-info${props.task.completed ? ' completed' : ''}`} onClick={() => props.toggleTask(props.task.id)}>
+        <div className="check">
+        <input type="checkbox" />
+        { ' ' }
         <h3>{props.task.task}</h3>
+        </div>
       </div>
   );
 }
